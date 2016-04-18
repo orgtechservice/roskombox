3bdc9649ff6cbda742c2c71e0bb85cd537fbe497
WST <ilya.averkov@gmail.com>
2016-04-18 14:11:53 +0300

    Отображение графиков. Fixes #9

--- roskombox/tasks.py
+++ roskombox/tasks.py
@@ -22,7 +22,17 @@ PYTHON_VENV_BIN = expanduser("~/venvs/roskombox/bin/python")
 
 # Путь к кольцевой базе данных
 RRD_FILENAME = "%s/stat.rrd" % settings.ROSKOM_CACHE_ROOT
-RRD_GRAPH_FILENAME = "%s/stat.png" % settings.MEDIA_ROOT
+
+# Пути к графикам
+RRD_GRAPH_SCANS = "%s/scans.png" % settings.MEDIA_ROOT
+RRD_GRAPH_TOTAL_URLS = "%s/total_urls.png" % settings.MEDIA_ROOT
+RRD_GRAPH_REGISTRY_FILESIZE = "%s/registry_filesize.png" % settings.MEDIA_ROOT
+RRD_GRAPH_TIMES = "%s/times.png" % settings.MEDIA_ROOT
+
+# Временные интервалы
+ONE_DAY = 86400
+ONE_MONTH = 2592000
+ONE_YEAR = 31536000
 
 def make_command(command, mode):
 	if mode == 'manual':
@@ -95,17 +105,66 @@ def last_scan_stats():
 def render_graphs():
 	print('Updating graph')
 	end_time = int(time.time())
-	start_time = end_time - (60*60*24)
+	start_time = end_time - ONE_MONTH
+
 	def_total_urls = 'DEF:total_urls=%s:total_urls:MAX' % RRD_FILENAME
+	def_available_urls = 'DEF:available_urls=%s:available_urls:MAX' % RRD_FILENAME
+	def_unavailable_urls = 'DEF:unavailable_urls=%s:unavailable_urls:MAX' % RRD_FILENAME
+	def_local_urls = 'DEF:local_urls=%s:local_urls:MAX' % RRD_FILENAME
+	def_registry_filesize = 'DEF:registry_filesize=%s:registry_filesize:MAX' % RRD_FILENAME
+	def_download_time = 'DEF:download_time=%s:download_time:MAX' % RRD_FILENAME
+	def_scan_time = 'DEF:scan_time=%s:scan_time:MAX' % RRD_FILENAME
 	
 	rrdtool.graph (
-		RRD_GRAPH_FILENAME,
+		RRD_GRAPH_SCANS,
 		'--border', '0',
-		'-t', 'Реестр Роскомнадзора',
+		'-t', 'Результаты проверок достпности',
 		'-s', str(start_time),
 		'-e', str(end_time),
+		'-w', '300',
+		'-h', '100',
+		def_available_urls,
+		def_local_urls,
+		'LINE1:available_urls#AA0000:Число доступных URL:',
+		'LINE1:local_urls#AAAA00:Число резолвящихся в локальные IP:'
+	)
+
+	rrdtool.graph (
+		RRD_GRAPH_TOTAL_URLS,
+		'--border', '0',
+		'-t', 'Общее число ссылок в реестре',
+		'-s', str(start_time),
+		'-e', str(end_time),
+		'-w', '300',
+		'-h', '100',
 		def_total_urls,
-		'LINE1:total_urls#AA0000:Число ссылок в реестре'
+		'AREA:total_urls#00AA00:Ссылок в реестре:',
+	)
+
+	rrdtool.graph (
+		RRD_GRAPH_REGISTRY_FILESIZE,
+		'--border', '0',
+		'-t', 'Объём выгрузки в байтах',
+		'-s', str(start_time),
+		'-e', str(end_time),
+		'-w', '300',
+		'-h', '100',
+		def_registry_filesize,
+		'AREA:registry_filesize#00AA00:Объём выгрузки:',
+	)
+
+	rrdtool.graph (
+		RRD_GRAPH_TIMES,
+		'--border', '0',
+		'-t', 'Временные затраты',
+		'-s', str(start_time),
+		'-e', str(end_time),
+		'-w', '300',
+		'-h', '100',
+		def_download_time,
+		def_scan_time,
+		'LINE1:download_time#AA0000:Длительность выгрузки:',
+		'LINE1:scan_time#0000AA:Длительность проверки:',
 	)
 
 def create_rrd():
@@ -144,6 +203,7 @@ def update_stats():
 	try:
 		update_rrd()
 	except:
+		print('Creating RRD')
 		create_rrd()
 
 	render_graphs()
